b31962fc1e3b7494da7a08a245c4b208d6e6fcc7
LeeWongSnail <@wangli_0632@163.com>
2016-11-04 10:48:06 +0800

    init

---
+++ ALiAlertView/ALiAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiAlertView
+//
+//  Created by LeeWong on 2016/11/4.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
