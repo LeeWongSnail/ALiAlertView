89c4dae9020a77b5d9a00fdcf0578d46c1b196b5
LeeWongSnail <@wangli_0632@163.com>
2016-11-04 10:51:47 +0800

    init

---
+++ ALiAlertView/ALiAlertView/ALiAlertView/ALiAlertView.h
@@ -0,0 +1,13 @@
+//
+//  ALiAlertView.h
+//  ALiAlertView
+//
+//  Created by LeeWong on 2016/11/4.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALiAlertView : UIView
+
+@end
---
+++ ALiAlertView/ALiAlertView/ALiAlertView/ALiAlertView.m
@@ -0,0 +1,21 @@
+//
+//  ALiAlertView.m
+//  ALiAlertView
+//
+//  Created by LeeWong on 2016/11/4.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import "ALiAlertView.h"
+
+@implementation ALiAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
